5324047964db4f176c956a3909a15f8f8c28e24e
RomuloRPS <romulo.rps@hotmail.com>
2020-03-30 02:32:38 -0300

    correção exportação de worker e queue

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,9 @@
 // export { _Model as Model };
 
 import { SchedulableTask as _SchedulableTask } from "./SchedulableTask";
+import { Queue as _Queue } from "./Queue";
+import { Worker as _Worker } from "./Worker";
 export { _SchedulableTask as SchedulableTask };
+export { _Queue as Queue };
+export { _Worker as Worker };
 
